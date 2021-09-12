498b90edcc1e3843a33fd2d4e7fa6df2c527f27a
Martin Acosta <tinchoz49@gmail.com>
2021-09-12 10:32:17 -0300

    added esbuild-plugin-brode

---
+++ packages/esbuild-plugin-brode/src/global.js
@@ -0,0 +1,18 @@
+import globalObj from '@geut/browser-node-core/global'
+
+export const global = globalObj
+
+export const globalThis = globalObj
+
+export const Buffer = globalObj.Buffer
+
+export const process = globalObj.process
+
+export const setTimeout = globalObj.setTimeout
+export const clearTimeout = globalObj.clearTimeout
+
+export const setInterval = globalObj.setInterval
+export const clearInterval = globalObj.clearInterval
+
+export const setImmediate = globalObj.setImmediate
+export const clearImmediate = globalObj.clearImmediate
