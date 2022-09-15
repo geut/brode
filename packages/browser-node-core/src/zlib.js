9b236462b09395f77dbd75b2b85c36f96dbeb021
Martín Acosta <tinchoz49@gmail.com>
2022-09-15 17:34:48 -0300

    improve compatibility between zlib and fflate

--- packages/browser-node-core/src/zlib.js
+++ packages/browser-node-core/src/zlib.js
@@ -1,16 +1,123 @@
-import { deflateSync, inflateSync, gzip, gunzip, zlibSync, unzlibSync } from './esm/fflate.js'
+import * as fflate from './esm/fflate.js'
 
-export const deflateRaw = deflateSync
-export const inflateRaw = inflateSync
-export const deflate = zlibSync
-export const inflate = unzlibSync
-export { gzip, gunzip }
+function isBuffer (b) {
+  return b != null && b._isBuffer === true
+}
+
+function handleCallbackBuffer (from, cb) {
+  return (err, buf) => {
+    if (buf && isBuffer(from)) return cb(err, Buffer.from(buf))
+    cb(err, buf)
+  }
+}
+
+function handleBuffer (from, buf) {
+  if (buf && isBuffer(from)) return buf
+  return buf
+}
+
+export const constants = {}
+
+export function deflate (buf, options = {}, cb) {
+  if (typeof options === 'function') {
+    cb = options
+    options = {}
+  }
+
+  const { level, memLevel: mem, ...fflateOpts } = options
+  return fflate.deflate(buf, { level, mem, ...fflateOpts }, handleCallbackBuffer(buf, cb))
+}
+
+export function deflateSync (buf, options = {}) {
+  const { level, memLevel: mem, ...fflateOpts } = options
+  return handleBuffer(fflate.deflateSync(buf, { level, mem, ...fflateOpts }))
+}
+
+export const deflateRaw = deflate
+export const deflateRawSync = deflateSync
+
+export function gunzip (buf, options = {}, cb) {
+  if (typeof options === 'function') {
+    cb = options
+    options = {}
+  }
+  return fflate.gunzip(buf, options, handleCallbackBuffer(buf, cb))
+}
+
+export function gunzipSync (buf, options = {}, cb) {
+  if (typeof options === 'function') {
+    cb = options
+    options = {}
+  }
+  return fflate.gunzipSync(buf, options, handleCallbackBuffer(buf, cb))
+}
+
+export function gzip (buf, options, cb) {
+  if (typeof options === 'function') {
+    cb = options
+    options = {}
+  }
+
+  const { level, memLevel: mem, ...fflateOpts } = options
+  return fflate.gzip(buf, { level, mem, ...fflateOpts }, handleCallbackBuffer(buf, cb))
+}
+
+export function gzipSync (buf, options = {}) {
+  const { level, memLevel: mem, ...fflateOpts } = options
+  return handleBuffer(buf, fflate.gzipSync(buf, { level, mem, ...fflateOpts }))
+}
+
+export function inflate (buf, options = {}, cb) {
+  if (typeof options === 'function') {
+    cb = options
+    options = {}
+  }
+
+  const { chunkSize: size, ...fflateOpts } = options
+  return fflate.inflate(buf, { size, ...fflateOpts }, handleCallbackBuffer(buf, cb))
+}
+
+export function inflateSync (buf, options = {}) {
+  const { chunkSize: size, ...fflateOpts } = options
+  return handleBuffer(buf, fflate.inflateSync(buf, { size, ...fflateOpts }))
+}
+
+export const inflateRaw = inflate
+export const inflateRawSync = inflateSync
+
+export function unzip (buf, options = {}, cb) {
+  if (typeof options === 'function') {
+    cb = options
+    options = {}
+  }
+  return fflate.unzip(buf, options, handleCallbackBuffer(buf, cb))
+}
+
+export function unzipSync (buf, options = {}, cb) {
+  if (typeof options === 'function') {
+    cb = options
+    options = {}
+  }
+  return fflate.unzipSync(buf, options, handleCallbackBuffer(buf, cb))
+}
+
+export { fflate }
 
 export default {
-  deflateRaw,
-  inflateRaw,
-  gzip,
-  gunzip,
+  constants,
+  fflate,
   deflate,
-  inflate
+  deflateSync,
+  deflateRaw,
+  deflateRawSync,
+  gunzip,
+  gunzipSync,
+  gzip,
+  gzipSync,
+  inflate,
+  inflateSync,
+  inflateRaw,
+  inflateRawSync,
+  unzip,
+  unzipSync
 }
